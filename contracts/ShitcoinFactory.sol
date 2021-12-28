4c4c6affa383edd5bc7696b9e9d6c391dea250ea
Richard Kovacs <richardkovacs.it@gmail.com>
2021-12-28 23:54:02 +0100

    feat(factory): save created addresses

--- contracts/ShitcoinFactory.sol
+++ contracts/ShitcoinFactory.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.10;
 import "./Shitcoin.sol";
 
 contract ShitcoinFactory {
+	Shitcoin[] private shitcoins;
+
     function create(
         string memory name,
         string memory symbol,
         uint256 totalSupply
-    ) public returns (address) {
+    ) public returns (Shitcoin) {
         Shitcoin shitcoin = new Shitcoin(name, symbol, totalSupply);
+		shitcoins.push(shitcoin);
 
-        return shitcoin.owner();
+        return shitcoin;
     }
+
+	function numberOfCoins() public view returns (uint256) {
+		return shitcoins.length;
+	}
+
+	function getShitcoin(uint256 i) public view returns (Shitcoin) {
+		return shitcoins[i];
+	}
 }
