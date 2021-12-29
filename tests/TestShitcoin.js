55700f217f900ed4048592accb4861d303501784
Richard Kovacs <richardkovacs.it@gmail.com>
2021-12-29 20:45:33 +0100

    refactor(test): use ethereum waffle

--- tests/TestShitcoin.js
+++ tests/TestShitcoin.js
@@ -1,8 +1,19 @@
 const Shitcoin = artifacts.require('Shitcoin');
 const truffleAssert = require('truffle-assertions');
+const ethereumWaffle = require('ethereum-waffle');
+const chai = require('chai');
+
+chai.use(ethereumWaffle.solidity);
 
 contract('Shitcoin', (accounts) => {
+  const expect = chai.expect;
   const nullAddress = '0x0000000000000000000000000000000000000000';
+  const [wallet, walletTo, otherWallet] = new ethereumWaffle.MockProvider().getWallets();
+  let token;
+
+  beforeEach(async () => {
+    token = await ethereumWaffle.deployContract(wallet, Shitcoin, ['Test coin', 'TEST', 1]);
+  });
 
   it('Creation should emit Transfer event', async () => {
     const coin = await Shitcoin.new('Test coin', 'TEST', 1, { from: accounts[0] });
@@ -15,16 +26,21 @@ contract('Shitcoin', (accounts) => {
   });
 
   it('Transfer should fail to null address', async () => {
-    const coin = await Shitcoin.new('Test coin', 'TEST', 1);
-
-    await truffleAssert.reverts(coin.transfer(nullAddress, 1), 'BEP20: transfer to the zero address');
+    await expect(token.transfer(nullAddress, 1)).to.be.revertedWith('BEP20: transfer to the zero address');
   });
 
   it('Transfer should fail on low balance', async () => {
-    const coin = await Shitcoin.new('Test coin', 'TEST', 1);
-
-    await truffleAssert.reverts(coin.transfer(accounts[0], 2), 'BEP20: transfer amount exceeds balance');
+    await expect(token.transfer(accounts[0], 2)).to.be.revertedWith('BEP20: transfer amount exceeds balance');
   });
 
-});
+  it('Transfer should work', async () => {
+    await expect(() => token.transfer(walletTo.address, 1))
+      .to.changeTokenBalances(token, [wallet, walletTo], [-1, 1]);
+  });
 
+  it('Transfer should emit Transfer event', async () => {
+    await expect(token.transfer(walletTo.address, 1))
+      .to.emit(token, 'Transfer')
+      .withArgs(wallet.address, walletTo.address, 1);
+  });
+});
