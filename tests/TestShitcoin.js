ba8231174ce5193338945518ab7572cfcb163ca1
Richard Kovacs <richardkovacs.it@gmail.com>
2021-12-29 21:50:10 +0100

    test(bep20): add more tests

--- tests/TestShitcoin.js
+++ tests/TestShitcoin.js
@@ -43,4 +43,58 @@ contract('Shitcoin', (accounts) => {
       .to.emit(token, 'Transfer')
       .withArgs(wallet.address, walletTo.address, 1);
   });
+
+  it('Approve should fail to null address', async () => {
+    await expect(token.approve(nullAddress, 5)).to.be.revertedWith('BEP20: approve to the zero address');
+  });
+
+  it('Approve should set allowance', async () => {
+    await token.approve(walletTo.address, 5);
+    expect(await token.allowance(wallet.address, walletTo.address)).to.equal(5);
+  });
+
+  it('Approve should emit Approval event', async () => {
+    await expect(token.approve(walletTo.address, 5))
+      .to.emit(token, 'Approval')
+      .withArgs(wallet.address, walletTo.address, 5);
+  });
+
+  it('TransferFrom should fail on low allowance', async () => {
+    await token.transfer(walletTo.address, 1);
+    await expect(token.transferFrom(walletTo.address, otherWallet.address, 1)).to.be.revertedWith('BEP20: transfer amount exceeds allowance');
+  });
+
+  it('TransferFrom should work', async () => {
+    const tokenFromOtherWallet = token.connect(walletTo);
+    await token.transfer(walletTo.address, 1);
+    await tokenFromOtherWallet.approve(wallet.address, 1);
+    await expect(() => token.transferFrom(walletTo.address, otherWallet.address, 1))
+      .to.changeTokenBalances(token, [walletTo, otherWallet], [-1, 1]);
+  });
+
+  it('TransferFrom should decrease allowance', async () => {
+    const tokenFromOtherWallet = token.connect(walletTo);
+    await token.transfer(walletTo.address, 1);
+    await tokenFromOtherWallet.approve(wallet.address, 1);
+    await token.transferFrom(walletTo.address, otherWallet.address, 1);
+    expect(await token.allowance(wallet.address, walletTo.address)).to.equal(0);
+  });
+
+  it('IncreaseAllowance should work', async () => {
+    await token.approve(walletTo.address, 1);
+    await token.increaseAllowance(walletTo.address, 4);
+    expect(await token.allowance(wallet.address, walletTo.address)).to.equal(5);
+  });
+
+  it('DecreaseAllowance should work', async () => {
+    await token.approve(walletTo.address, 5);
+    await token.decreaseAllowance(walletTo.address, 4);
+    expect(await token.allowance(wallet.address, walletTo.address)).to.equal(1);
+  });
+  
+  it('DecreaseAllowance should fail when deceasing below zero', async () => {
+    await token.approve(walletTo.address, 1);
+    await expect(token.decreaseAllowance(walletTo.address, 2)).to.be.revertedWith('BEP20: decreased allowance below zero');
+  });
+
 });
