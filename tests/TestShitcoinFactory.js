6123160aff59df88178f43b1c06dc1021db4bac3
Richard Kovacs <richardkovacs.it@gmail.com>
2021-12-30 17:06:00 +0100

    test(factory): add missing factory tests

--- tests/TestShitcoinFactory.js
+++ tests/TestShitcoinFactory.js
@@ -1,5 +1,7 @@
 const ShitcoinFactory = artifacts.require('ShitcoinFactory');
+const Shitcoin = artifacts.require('Shitcoin');
 const ethereumWaffle = require('ethereum-waffle');
+const ethers = require('ethers');
 const chai = require('chai');
 
 chai.use(ethereumWaffle.solidity);
@@ -13,10 +15,33 @@ contract('ShitcoinFactory', () => {
     factory = await ethereumWaffle.deployContract(wallet, ShitcoinFactory);
   });
 
+  it('Create should create shitcoin', async () => {
+    await factory.create('Test token', 'TTN', 5);
+    const token = await factory.getShitcoin(0);
+    const contract = new ethers.Contract(token, Shitcoin.abi, wallet);
+
+    expect(await contract.callStatic.name()).to.equal('Test token');
+    expect(await contract.callStatic.symbol()).to.equal('TTN');
+    expect(await contract.callStatic.totalSupply()).to.equal(5);
+  });
+
   it('NumberOfCoins should return number of coins', async () => {
     await factory.create('Test token', 'TTN', 5);
     expect(await factory.numberOfCoins()).to.equal(1);
     await factory.create('Test coin', 'TEST', 5);
     expect(await factory.numberOfCoins()).to.equal(2);
+  });
+
+  it('GetShitcoin returns coin', async () => {
+    await factory.create('Token 1', 'TN1', 5);
+    await factory.create('Token 2', 'TN2', 7);
+
+    const token1 = await factory.getShitcoin(0);
+    const token2 = await factory.getShitcoin(1);
+    const contract1 = new ethers.Contract(token1, Shitcoin.abi, wallet);
+    const contract2 = new ethers.Contract(token2, Shitcoin.abi, wallet);
+
+    expect(await contract1.callStatic.symbol()).to.equal('TN1');
+    expect(await contract2.callStatic.symbol()).to.equal('TN2');
   });
 });
