1c5e62e611ebf10a0dbeb7b4167595e3feb92b9c
Richard Kovacs <richardkovacs.it@gmail.com>
2022-02-05 17:42:54 +0100

    feat(factory): write abi to file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const ShitcoinFactory = artifacts.require("ShitcoinFactory");
+const fs = require('fs');
 
 module.exports = async (callback) => {
 	let factory = await ShitcoinFactory.deployed();
+	const abiJson = 'factory-abi.json';
+
+	fs.writeFileSync(abiJson, JSON.stringify(factory.abi));
+
 	callback();
 }
