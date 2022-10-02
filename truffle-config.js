5456d3a17441b9f8fd85b95ee7a568e46691af25
Richard Kovacs <richardkovacs.it@gmail.com>
2022-10-02 22:44:42 +0200

    build: add ethereum network config

--- truffle-config.js
+++ truffle-config.js
@@ -73,6 +73,18 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    ethereum: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        `https://mainnet.infura.io/v3/${infuraProjectId}`),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 5000000000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
