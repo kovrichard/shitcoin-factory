1a4b3774fcf2aa3094c748d1e53eec7488369a7d
Richard Kovacs <richardkovacs.it@gmail.com>
2022-10-08 09:47:12 +0200

    feat(run): use ethers in index js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const ShitcoinFactory = artifacts.require("ShitcoinFactory");
 const fs = require('fs');
+const ethers = require('ethers');
+const abi = JSON.parse(fs.readFileSync('./build/contracts/ShitcoinFactory.json'));
+const caddress = '0x0000000000000000000000000000000000000000';
+
+provider = new ethers.providers.InfuraProvider('sepolia');
+let wallet = new ethers.Wallet.fromMnemonic('');
+let signer = wallet.connect(provider);
+let contract = new ethers.Contract(caddress, abi.abi, signer);
+
 
 module.exports = async (callback) => {
-	let factory = await ShitcoinFactory.deployed();
+	console.log(await contract.costAddress());
+	console.log(await contract.getCost());
 
-	await factory.setCostAddress('0x0000000000000000000000000000000000000000');
-	await factory.setCost(BigInt(10 * 10 ** 18));
-	console.log(await factory.costAddress());
-	console.log(await factory.getCost());
+	//await contract.setCostAddress('0x0000000000000000000000000000000000000000');
+	//await contract.setCost(BigInt(10 * 10 ** 18));
 
 	callback();
 }
