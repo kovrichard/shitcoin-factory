59b82508fa8bbdb519d1c935174490e2c533689c
Richard Kovacs <richardkovacs.it@gmail.com>
2022-10-08 09:41:31 +0200

    build: add sepolia testnet

--- truffle-config.js
+++ truffle-config.js
@@ -73,6 +73,17 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        `https://sepolia.infura.io/v3/${infuraProjectId}`),
+      network_id: 11155111,
+      gas: 6721975,        // Ropsten has a lower block limit than mainnet
+      gasPrice: 20000000000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     ethereum: {
       provider: () => new HDWalletProvider(
         mnemonic,
